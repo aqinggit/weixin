fc314ed22feeae73de8f34ecd2d066da3bbfcc3b
QazseWong <w@qiiing.com>
2019-07-31 17:28:17 +0800

    志愿者页面推进

--- protected/controllers/WeixinController.php
+++ protected/controllers/WeixinController.php
@@ -1,23 +1,25 @@
 <?php
 
 /**
- * @filename WeixinController.php 
+ * @filename WeixinController.php
  * @Description 微信登陆
- * @author 阿年飞少 <ph7pal@qq.com> 
- * @link http://www.newsoul.cn 
- * @copyright Copyright©2015 阿年飞少 
- * @datetime 2015-5-29  10:03:15 
+ * @author 阿年飞少 <ph7pal@qq.com>
+ * @link http://www.newsoul.cn
+ * @copyright Copyright©2015 阿年飞少
+ * @datetime 2015-5-29  10:03:15
  */
 Yii::import('application.vendors.weixin.*');
 require_once 'weixin.class.php';
 
-class WeixinController extends Q {
+class WeixinController extends Q
+{
 
     private $cookieTime = 86400;
     private $wx;
     private $currentAction;
 
-    public function init() {
+    public function init()
+    {
         parent::init();
         $wxId = zmf::config('weixin_app_id');
         $wxSecret = zmf::config('weixin_app_key');
@@ -28,19 +30,20 @@ class WeixinController extends Q {
         }
         $from = zmf::val('from', 1); //来源，官网和合作商
         if (!in_array($from, array('web', 'mcenter'))) {
-            $from='web';
+            $from = 'web';
         }
 //        if (!$wxId || !$wxSecret || !$callback) {
 //            throw new CHttpException(404, '暂未开放');
 //        }
         $referer = zmf::val('referer', 1);
         $this->referer = $referer ? $referer : $this->referer;
-        $callback.='?action=' . $this->currentAction . '&from='.$from.'&referer=' . $this->referer;
+        $callback .= '?action=' . $this->currentAction . '&from=' . $from . '&referer=' . $this->referer;
         $this->wx = new weixinSdk($wxId, $wxSecret, $callback);
-        $this->pageTitle=  '微信 - '.zmf::config('sitename');
+        $this->pageTitle = '微信 - ' . zmf::config('sitename');
     }
 
-    public function actionReg() {
+    public function actionReg()
+    {
         if (!Yii::app()->user->isGuest) {
             $this->redirect($this->referer);
         }
@@ -50,47 +53,26 @@ class WeixinController extends Q {
 
         $this->pageTitle = '欢迎注册-' . zmf::config('sitename');
         $this->mobileTitle = '注册';
-        $this->currentModule='login';
+        $this->currentModule = 'login';
 
-        $model=new Users;
-        if(isset($_POST['Users']))
-        {
-            $model->attributes=$_POST['Users'];
-            if($model->validate())
-            {
-                return '注册完成';
+        $model = new Users;
+        if (isset($_POST['Users'])) {
+            $model->attributes = $_POST['Users'];
+            zmf::test($_POST['Users']);
+            if ($model->validate()) {
+                if ($model->save()) {
+                    echo '注册完成';
+                } else {
+                    $this->message(503, '系统原因,注册失败!', $this->referer);
+                }
             }
         }
-        $this->render('reg1',array('model'=>$model));
+        $this->render('reg', array('model' => $model));
 
     }
 
-    public function actionReg1()
+    public function actionIndex()
     {
-        $model=new Users;
-
-        // uncomment the following code to enable ajax-based validation
-        /*
-        if(isset($_POST['ajax']) && $_POST['ajax']==='users-reg1-form')
-        {
-            echo CActiveForm::validate($model);
-            Yii::app()->end();
-        }
-        */
-
-        if(isset($_POST['Users']))
-        {
-            $model->attributes=$_POST['Users'];
-            if($model->validate())
-            {
-                // form inputs are valid, do something here
-                return;
-            }
-        }
-        $this->render('reg1',array('model'=>$model));
-    }
-
-    public function actionIndex() {
         $action = zmf::val('action', 1); //操作类型，是登录、注册、绑定、投票
         //array('login', 'reg', 'bind', 'vote'))
         if (!in_array($action, array('getInfo', 'login', 'reg', 'bind'))) {
@@ -98,19 +80,20 @@ class WeixinController extends Q {
         }
         $from = zmf::val('from', 1); //来源，官网和合作商
         if (!in_array($from, array('web', 'mcenter'))) {
-            $from='web';
+            $from = 'web';
         }
-        if (in_array($action, array('login', 'reg')) && $this->uid && $from=='web') {
+        if (in_array($action, array('login', 'reg')) && $this->uid && $from == 'web') {
             $this->message(0, '您已登录，请勿该操作', $this->referer);
-        } elseif ($action == 'bind' && !$this->uid && $from=='web') {
+        } elseif ($action == 'bind' && !$this->uid && $from == 'web') {
             $this->redirect(array('site/login'));
         }
-        zmf::setCookie('lastWeixinAction', $action, $this->cookieTime);        
+        zmf::setCookie('lastWeixinAction', $action, $this->cookieTime);
         $loginurl = $this->wx->login();
         $this->redirect($loginurl);
     }
 
-    public function actionCallback() {
+    public function actionCallback()
+    {
         $wx_data = zmf::getCookie('userWeixinData');
         if (!$wx_data) {
             $res = $this->wx->getAccessToken();
@@ -128,14 +111,14 @@ class WeixinController extends Q {
             $data = array_merge($token, $uinfo);
             $strdata = serialize($data);
             zmf::setCookie('userWeixinData', $strdata, $this->cookieTime);
-            zmf::setFCache('userWeixinData-'.$data['openid'], $strdata, $this->cookieTime);
+            zmf::setFCache('userWeixinData-' . $data['openid'], $strdata, $this->cookieTime);
         } else {
             $data = unserialize($wx_data);
             $strdata = $wx_data;
-            zmf::setFCache('userWeixinData-'.$data['openid'], $wx_data, $this->cookieTime);
+            zmf::setFCache('userWeixinData-' . $data['openid'], $wx_data, $this->cookieTime);
         }
         //0415添加，微信回调后都写入数据库
-        $uinfo = Weixin::model()->find('openid=:uid',array(':uid' => $data['openid']));
+        $uinfo = Weixin::model()->find('openid=:uid', array(':uid' => $data['openid']));
         $attr = array(
             'openid' => $data['openid'],
             'unionid' => $data['unionid'],
@@ -162,9 +145,9 @@ class WeixinController extends Q {
         }
         $from = zmf::val('from', 1); //来源，官网和合作商
         if (!in_array($from, array('web', 'mcenter'))) {
-            $from='web';
+            $from = 'web';
         }
-        if($from=='mcenter'){
+        if ($from == 'mcenter') {
             $urlData = array(
                 'openid' => $data['openid'],
                 'action' => $action,
@@ -176,7 +159,7 @@ class WeixinController extends Q {
         if (in_array($action, array('login', 'reg')) && $this->uid) {
             $this->message(0, '您已登录，请勿该操作', $this->referer);
         } elseif ($action == 'bind' && !$this->uid) {
-            $this->redirect(array('site/login','bind'=>'weixin'));
+            $this->redirect(array('site/login', 'bind' => 'weixin'));
         }
         $openid = $data['openid'];
         if (!$openid) {
@@ -212,11 +195,11 @@ class WeixinController extends Q {
                 if (empty($bindInfo)) {
                     throw new CHttpException(404, '获取微信信息错误，请重试');
                 } elseif ($bindInfo['uid'] == $this->uid) {
-                    Users::model()->updateByPk($uid, array('bindWeixin'=>1));
+                    Users::model()->updateByPk($uid, array('bindWeixin' => 1));
                     $this->redirect($this->referer);
                 } else {
                     if (Weixin::model()->updateByPk($bindInfo['id'], array('uid' => $uid))) {
-                        Users::model()->updateByPk($uid, array('bindWeixin'=>1));
+                        Users::model()->updateByPk($uid, array('bindWeixin' => 1));
                         $this->redirect($this->referer);
                     } else {
                         throw new CHttpException(404, '写入数据时错误，请重试');
@@ -228,7 +211,8 @@ class WeixinController extends Q {
         }
     }
 
-    private function loginWithWeixin($bindInfo, $data = array()) {
+    private function loginWithWeixin($bindInfo, $data = array())
+    {
         $userInfo = Users::getOne($bindInfo['uid']);
         if ($userInfo) {
             $_identity = new U($userInfo['phone'], $userInfo['password']);
@@ -248,12 +232,13 @@ class WeixinController extends Q {
      * 删除当前用户的绑定
      * @throws CHttpException
      */
-    public function actionDel() {
+    public function actionDel()
+    {
         $uid = $this->uid;
         if (!$uid) {
             $this->redirect(array('site/login'));
         }
-        if (Weixin::model()->updateAll(array('uid'=>0),'uid=:uid',array(':uid'=>$uid))) {
+        if (Weixin::model()->updateAll(array('uid' => 0), 'uid=:uid', array(':uid' => $uid))) {
             $this->redirect(array('users/setting'));
         } else {
             throw new CHttpException(404, '解绑失败，请重试');
