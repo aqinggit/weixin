f0e4081191931b453e7aa7b0191178bdd43be01d
Wong <13340685430@163.com>
2019-08-02 14:46:12 +0800

    前台页面完善

---
+++ themes/mobile/views/article/articleDedail.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wang
+ * Date: 2019/8/2
+ * Time: 13:20
+ */
--- themes/mobile/views/index/index.php
+++ themes/mobile/views/index/index.php
@@ -1,1 +1,111 @@
-<a href="javascript:;" class="weui-btn weui-btn_primary">注册</a>
+<!--<a href="javascript:;" class="weui-btn weui-btn_primary">登录</a>-->
+<style>
+
+    .fs12 {
+        font-size: 12px;
+    }
+
+    .fs16 {
+        font-size: 16px;
+    }
+
+    .active_img {
+        width: 120px;
+        height: 80px;
+        margin-right: 15px;
+    }
+
+    .article_detail {
+        padding: 10px 15px;
+    }
+
+    .article_detail img {
+        display: block;
+        width: 100%;
+        margin: 15px 0;
+    }
+    .article_detail p{
+        text-align: justify;
+    }
+</style>
+<!--文章列表-->
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题</p>
+        </div>
+        <div class="weui-cell__ft fs12">2019-07-20</div>
+    </div>
+</div>
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+        <div class="weui-cell__ft fs12">2019-07-20</div>
+    </div>
+</div>
+
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__hd"><img class="active_img"
+                                        src="http://s.zhiyuanyun.com/www.zycq.org/cms/201906/11/5cff14ab73fc1.png">
+        </div>
+        <div class="weui-cell__bd fs16" style="float: left">
+            <p>标题标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+
+    </div>
+    <div class="weui-cell">
+        <div class="weui-cell__hd"><img class="active_img"
+                                        src="http://s.zhiyuanyun.com/www.zycq.org/cms/201906/11/5cff14ab73fc1.png">
+        </div>
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+
+    </div>
+    <div class="weui-cell">
+        <div class="weui-cell__hd"><img class="active_img"
+                                        src="http://s.zhiyuanyun.com/www.zycq.org/cms/201906/11/5cff14ab73fc1.png">
+        </div>
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+
+    </div>
+</div>
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题</p>
+        </div>
+        <div class="weui-cell__ft fs12">2019-07-20</div>
+    </div>
+</div>
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+        <div class="weui-cell__ft fs12">2019-07-20</div>
+    </div>
+</div>
+<div class="weui-cells">
+    <div class="weui-cell">
+        <div class="weui-cell__bd fs16">
+            <p>标题标题标题标题标题标题标题标题标题标题标题</p>
+        </div>
+        <div class="weui-cell__ft fs12">2019-07-20</div>
+    </div>
+</div>
+<!--文章详情-->
+<div class="article_detail">
+    <p class="fs16">内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+    <img style="" src="http://s.zhiyuanyun.com/www.zycq.org/cms/201906/11/5cff14ab73fc1.png" alt="活动图片">
+    <p class="fs16">
+        内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+    <img style="" src="http://s.zhiyuanyun.com/www.zycq.org/cms/201906/11/5cff14ab73fc1.png" alt="活动图片">
+    <p class="fs16">
+        内容内容内容内内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+</div>
