c9515b26bfa9ca2a987de8fef78929e5e3a50008
Wong <13340685430@163.com>
2019-08-04 14:51:34 +0800

    1.列表更新 2.问答完善

---
+++ themes/mobile/views/competition/index.php
@@ -0,0 +1,124 @@
+<style>
+    .homepage{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background: url("http://cqfb.people.com.cn/h5/20190403zs/img/bg.jpg") no-repeat;
+        background-size: 100% 100%;
+
+
+    }
+    .img1 {
+        width: 280px;
+        height: 88px;
+        display: block;
+        margin: 42px auto 6px auto;
+    }
+
+    .organizer {
+        height: auto;
+        text-align: center;
+        font-weight: bold;
+        line-height: 1.5em;
+    }
+
+    .box {
+        width: 255px;
+        height: 162px;
+        margin: 110px auto 70px auto;
+        text-align: center;
+
+    }
+
+    .box p {
+        margin: 3px auto;
+    }
+
+    .icon_box {
+        width: 255px;
+        height: 88px;
+        display: flex;
+
+    }
+
+    .icon_box img {
+        width: 60px;
+        margin-right: 5px;
+    }
+
+    .phone {
+        padding: 10px 15px;
+        border: 1px solid rgba(0, 0, 0, .2);
+        border-radius: 3px;
+        outline: 0;
+        background-color: #fff;
+        -webkit-appearance: none;
+
+
+    }
+
+    .color-9b {
+        color: #9b9b9b;
+
+    }
+
+    .button01 {
+        z-index: 10;
+        width: 134px;
+        background: #c81f18;
+        color: #fff;
+        font-size: 20px;
+        text-align: center;
+        border-radius: 10px;
+        padding: 7px 0;
+        border: 1px solid #c81f18;
+        margin-right: 1%;
+    }
+
+    .button02 {
+        z-index: 10;
+        width: 134px;
+        background: #fdeee7;
+        border: 1px solid #c81f18;
+        color: #110f0f;
+        font-size: 20px;
+        text-align: center;
+        margin-left: 1%;
+        border-radius: 10px;
+        padding: 7px 0;
+    }
+    .bottom_img{
+        position: absolute;
+        height: 16%;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+    }
+
+</style>
+
+<div class="homepage">
+    <img src="http://cqfb.people.com.cn/h5/20190403zs/img/title.png" class="img1">
+    <div class="organizer">主办单位：</span><br>
+        <span>中共重庆市委组织部</span><br>
+        <span>中共重庆市委直属机关工作委员会</span><br>
+        <span>中共重庆市委当代党员杂志社</span>
+    </div>
+    <div class="box">
+        <div class="icon_box">
+            <img src="http://cqfb.people.com.cn/h5/20190403zs/img/p01.png">
+            <img src="http://cqfb.people.com.cn/h5/20190403zs/img/p02.png">
+            <img src="http://cqfb.people.com.cn/h5/20190403zs/img/p03.png">
+            <img src="http://cqfb.people.com.cn/h5/20190403zs/img/p04.png">
+        </div>
+        <p class="color-9b">请输入您的手机号码</p>
+        <input type="number" maxlength="11" class="phone color-9b"><br>
+    </div>
+    <div style="margin: 0 auto;width: 280px">
+        <button class="button01">我要参与</button>
+        <button class="button02">个人中心</button>
+    </div>
+    <div>
+        <img class="bottom_img" src="http://cqfb.people.com.cn/h5/20190403zs/img/bottom.png" alt="建國">
+    </div>
+</div>
