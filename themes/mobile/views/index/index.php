163018d9e5d7ef61e4c539c5943e7670992c9fb9
QazseWong <w@qiiing.com>
2019-08-06 15:10:26 +0800

    首页调整

--- themes/mobile/views/index/index.php
+++ themes/mobile/views/index/index.php
@@ -16,41 +16,41 @@
     }
 </style>
 <div class="weui-tab">
-                <div class="swiper-slide"><img src="http://s.zhiyuanyun.com/www.chinavolunteer.cn/cms/201807/09/5b4320e279547.JPG" style="width: 400px" alt=""></div>
-                <div class="img_title">北京：首届阳光助残志愿服务技能大赛决赛今天举行</div>
+    <div class="swiper-slide"><img src="http://s.zhiyuanyun.com/www.chinavolunteer.cn/cms/201807/09/5b4320e279547.JPG"
+                                   style="width: 400px" alt=""></div>
+    <div class="img_title">通知:系统测试中...</div>
 
-        <div class="weui-grids">
-            <a href="/app/weixin/register.php" class="weui-grid js_grid">
-                <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_zyz.png"></div>
-                <p class="weui-grid__label">成为志愿者</p>
-            </a>
-            <a href="/app/weixin/near.php?type=org_list" class="weui-grid js_grid">
-                <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_tt.png"></div>
-                <p class="weui-grid__label">志愿团体</p>
-            </a>
-            <a href="/app/weixin/near.php?type=opp_list" class="weui-grid js_grid">
-                <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_xm.png"></div>
-                <p class="weui-grid__label">知识竞赛</p>
-            </a>
-        </div>
+    <div class="weui-grids">
+        <a href="<?php echo zmf::createUrl('/weixin/reg')?>" class="weui-grid js_grid">
+            <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_zyz.png"></div>
+            <p class="weui-grid__label">成为志愿者</p>
+        </a>
+        <a href="<?php echo zmf::createUrl('/activity/index')?>" class="weui-grid js_grid">
+            <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_tt.png"></div>
+            <p class="weui-grid__label">志愿活动</p>
+        </a>
+        <a href="<?php echo zmf::createUrl('/Competition/index')?>" class="weui-grid js_grid">
+            <div class="weui-grid__icon"><img src="https://css.zhiyuanyun.com/default/wx/images/gird_xm.png"></div>
+            <p class="weui-grid__label">知识竞赛</p>
+        </a>
     </div>
-    <div class="clearfix" style="height:65px;"></div>
-    <div class="weui-tabbar">
-        <a href="/app/weixin/index.php" class="weui-tabbar__item weui-bar__item_on">
-            <img src="https://css.zhiyuanyun.com/default/wx/images/tab.home.press.png" alt="" class="weui-tabbar__icon">
-            <p class="weui-tabbar__label">首页</p>
-        </a>
-        <a href="/app/weixin/qrscan.php" class="weui-tabbar__item ">
-            <img src="https://css.zhiyuanyun.com/default/wx/images/gird_xj.png" alt=""
-                 class="weui-tabbar__icon">
-            <p class="weui-tabbar__label">志愿活动</p>
-        </a>
+</div>
+<div class="clearfix" style="height:65px;"></div>
+<div class="weui-tabbar">
+    <a href="<?php echo zmf::createUrl('/index/index')?>" class="weui-tabbar__item weui-bar__item_on">
+        <img src="https://css.zhiyuanyun.com/default/wx/images/tab.home.press.png" alt="" class="weui-tabbar__icon">
+        <p class="weui-tabbar__label">首页</p>
+    </a>
+    <a href="<?php echo zmf::createUrl('/Competition/index')?>" class="weui-tabbar__item ">
+        <img src="https://css.zhiyuanyun.com/default/wx/images/gird_xj.png" alt=""
+             class="weui-tabbar__icon">
+        <p class="weui-tabbar__label">志愿活动</p>
+    </a>
 
-        <a href="/app/weixin/my.php" class="weui-tabbar__item ">
-            <img src="https://css.zhiyuanyun.com/default/wx/images/tab.user.normal.png" alt=""
-                 class="weui-tabbar__icon">
-            <p class="weui-tabbar__label">我的</p>
-        </a>
+    <a href="<?php echo zmf::createUrl('/weixin/index')?>" class="weui-tabbar__item ">
+        <img src="https://css.zhiyuanyun.com/default/wx/images/tab.user.normal.png" alt=""
+             class="weui-tabbar__icon">
+        <p class="weui-tabbar__label">我的</p>
+    </a>
 
-    </div>
 </div>
--- themes/mobile/views/layouts/main.php
+++ themes/mobile/views/layouts/main.php
@@ -2,8 +2,5 @@
 <section class="ui-container">
     <?php echo $content; ?>
 </section>
-<div class="text-center" style="padding: 20px 0;background: #f2f2f2;margin-bottom: 45px">
-    <p class="color-666"><span class="yen"><?php echo zmf::config('copyright');?></span><span><?php echo zmf::config('sitename');?></span></p>
-    <p><a href="http://www.miibeian.gov.cn/" target="_blank" class="color-666" rel="nofollow"><?php echo zmf::config('beian');?></a></p>
-</div>
+
 <?php $this->renderPartial('/layouts/_nav');echo zmf::config('tongji');$this->endContent();
