5e21694720604e375a007fdd1dd4c965d5301387
Wong <13340685430@163.com>
2019-08-07 17:10:20 +0800

    web端封面图修改

--- themes/web/views/index/index.php
+++ themes/web/views/index/index.php
@@ -1,1 +1,16 @@
-<img src="<?php echo zmf::config('baseurl') . 'jsCssSrc/images/face.png' ?>" alt="封面图">
+<style>
+    .index_banner {
+        position: relative;
+        overflow: hidden;
+        height: 750px;
+        padding: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+<div class="index_banner"
+     style="background: url('<?php echo zmf::config('baseurl') . 'jsCssSrc/images/face.png' ?>');height: 750px">
+</div>
